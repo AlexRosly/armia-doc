0345a4c9fe3152299a1f1e221383786bc7c9d183
AlexRosly <rosly.a777@gmail.com>
2026-09-13 17:02:56 +0300

    update buildPropertyGroups for report

--- src/services/documents/report/buildPropertyGroups.js
+++ src/services/documents/report/buildPropertyGroups.js
@@ -1,96 +1,9 @@
-// const { buildPropertyText } = require("../shared");
-
-// const buildPropertyGroups = (lostProperty = []) => {
-//   return lostProperty.map((service) => ({
-//     service: service.service,
-
-//     subDivisions: service.subDivisions.map((subDivision) => ({
-//       subDivision: subDivision.subDivision,
-
-//       items: subDivision.listOfProperty.map((property, index, array) => ({
-//         text:
-//           buildPropertyText(property) +
-//           (index === array.length - 1 ? "." : ";"),
-//       })),
-//     })),
-//   }));
-// };
-
-// module.exports = buildPropertyGroups;
-// const { buildPropertyText } = require("../shared");
-
-// const stripEndingPunctuation = (value = "") => {
-//   return String(value)
-//     .replace(/[.;:,]+$/g, "")
-//     .trim();
-// };
-
-// const buildPropertyGroups = (lostProperty = []) => {
-//   return lostProperty.map((service) => ({
-//     service: service.service,
-
-//     subDivisions: service.subDivisions.map((subDivision) => ({
-//       subDivision: subDivision.subDivision,
-
-//       items: subDivision.listOfProperty.map((property, index, array) => {
-//         const baseText = stripEndingPunctuation(buildPropertyText(property));
-//         const ending = index === array.length - 1 ? "." : ";";
-
-//         return {
-//           text: `${baseText}${ending}`,
-//         };
-//       }),
-//     })),
-//   }));
-// };
-
-// module.exports = buildPropertyGroups;
-// const { buildPropertyText } = require("../shared");
-
-// const stripEndingPunctuation = (value = "") => {
-//   return String(value)
-//     .replace(/[.;:,]+$/g, "")
-//     .trim();
-// };
-
-// const buildPropertyGroups = (lostProperty = []) => {
-//   const totalItemsCount = lostProperty.reduce((total, service) => {
-//     return (
-//       total +
-//       (service.subDivisions || []).reduce((subTotal, subDivision) => {
-//         return subTotal + (subDivision.listOfProperty || []).length;
-//       }, 0)
-//     );
-//   }, 0);
-
-//   let globalItemIndex = 0;
-
-//   return lostProperty.map((service) => ({
-//     service: service.service,
-
-//     subDivisions: (service.subDivisions || []).map((subDivision) => ({
-//       subDivision: subDivision.subDivision,
-
-//       items: (subDivision.listOfProperty || []).map((property) => {
-//         globalItemIndex += 1;
-
-//         const baseText = stripEndingPunctuation(buildPropertyText(property));
-//         const ending = globalItemIndex === totalItemsCount ? "." : ";";
-
-//         return {
-//           text: `${baseText}${ending}`,
-//         };
-//       }),
-//     })),
-//   }));
-// };
-
-// module.exports = buildPropertyGroups;
 const { buildPropertyText } = require("../shared");
 
-const stripEndingPunctuation = (value = "") =>
+const stripEndingSeparators = (value = "") =>
   String(value)
-    .replace(/[.;:,]+$/g, "")
+    .trimEnd()
+    .replace(/[;:,]+$/g, "")
     .trim();
 
 const buildPropertyGroups = (lostProperty = []) => {
@@ -132,13 +45,13 @@ const buildPropertyGroups = (lostProperty = []) => {
           items: listOfProperty.map((property) => {
             globalItemIndex += 1;
 
-            const baseText = stripEndingPunctuation(
+            const baseText = stripEndingSeparators(
               buildPropertyText(property),
             );
             const ending = globalItemIndex === totalItemsCount ? "." : ";";
 
             return {
-              text: `${baseText}${ending}`,
+              text: `${ending === "." ? baseText.replace(/\.+$/, "") : baseText}${ending}`,
             };
           }),
         };
